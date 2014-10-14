46930a0a17700f5bcaa26da7f3eb047eaf6ecbf0
bluesroo <jpariseau101@gmail.com>
2014-10-13 23:13:10 -0700

    Needed to do some refactoring before moving forward. I've cut down on the repetition. Delete is a skeleton for now.

--- src/testing/JDBCManipulate.java
+++ src/testing/JDBCManipulate.java
@@ -19,33 +19,34 @@ public class JDBCManipulate {
     public static void main(String[] args) {
         //Initializing variables
         Connection conn = null;
-        Statement stmt = null;
+        Statement sqlStatment = null;
         Integer choice = 0;
+        String caller = "main";
 
         try {
             //Open connection
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
             //Defining statement
-            stmt = conn.createStatement();
+            sqlStatment = conn.createStatement();
             //Register driver
             Class.forName(JDBC_DRIVER);
 
             //Gets the user's choice and directs to the proper function
             while (choice != 4) {
-                printChoices();
+                printChoices(caller);
                 System.out.println("What would you like to do? ");
                 choice = getChoice();
                 System.out.println("\n");
                 switch (choice) {
                     case 1:
-                        getInfo(stmt);
+                        getInfo(sqlStatment);
                         break;
                     case 2:
-                        addEntry(stmt);
+                        addEntry(sqlStatment);
                         System.out.println("Data added.\n");
                         break;
                     case 3:
-                        deleteEntry();
+                        deleteEntry(sqlStatment);
                         System.out.println("Data deleted.\n");
                         break;
                     case 4:
@@ -65,8 +66,8 @@ public class JDBCManipulate {
         } finally {
             //finally block used to close resources
             try {
-                if (stmt != null) {
-                    stmt.close();
+                if (sqlStatment != null) {
+                    sqlStatment.close();
                 }
             } catch (SQLException se2) {
                 se2.printStackTrace();
@@ -81,11 +82,23 @@ public class JDBCManipulate {
         } //end try
     }//end main
 
-    static void printChoices() {
-        System.out.println("Press 1 to see database information.\n" +
-                "Press 2 to add an entry.\n" +
-                "Press 3 to delete an entry.\n" +
-                "Press 4 to exit.");
+    static void printChoices(String caller) {
+        switch (caller) {
+            case "main":
+                System.out.println("Press 1 to see database information.\n" +
+                        "Press 2 to add an entry.\n" +
+                        "Press 3 to delete an entry.\n" +
+                        "Press 4 to exit.");
+                break;
+            case "delete":
+                System.out.println("Press 1 to delete by name.\n" +
+                        "Press 2 to delete by id.\n" +
+                        "Press 4 to cancel.");
+                break;
+            default:
+                System.out.println("Invalid caller.");
+                break;
+        }
     }
 
     static int getChoice() {
@@ -100,6 +113,48 @@ public class JDBCManipulate {
             loops++;
         } while (choice < 1 || choice > 4);
         return choice;
+    }
+
+    static String getString(String warning, int minLength, int maxLength) {
+        String input;
+        Scanner in = new Scanner(System.in);
+        int loops = 0;
+        do {
+            if (loops > 1) {
+                System.out.println(warning);
+            }
+            input = in.next();
+            loops++;
+        } while (input.length() > maxLength || input.length() < minLength);
+        return input;
+    }
+
+    static String getInt(String warning, int min, int max) {
+        String input;
+        Scanner in = new Scanner(System.in);
+        int loops = 0;
+        do {
+            if (loops > 1) {
+                System.out.println(warning);
+            }
+            input = in.next();
+            loops++;
+        } while (Integer.parseInt(input) < min || Integer.parseInt(input) > max);
+        return input;
+    }
+
+    static String getFloat(String warning, Float min, Float max) {
+        String input;
+        Scanner in = new Scanner(System.in);
+        int loops = 0;
+        do {
+            if (loops > 1) {
+                System.out.println(warning);
+            }
+            input = in.next();
+            loops++;
+        } while (Float.parseFloat(input) < min || Float.parseFloat(input) > max);
+        return input;
     }
 
     static void getInfo(Statement sqlStatement) {
@@ -128,50 +183,22 @@ public class JDBCManipulate {
     static void addEntry(Statement sqlStatement) {
         String name, GPA, major, grad;
         Scanner in = new Scanner(System.in);
-        int loops = 0;
 
         //Get name
         System.out.println("Enter a first name: ");
-        do {
-            if (loops > 1) {
-                System.out.println("Please enter between 1 and 30 characters: ");
-            }
-            name = in.next();
-            loops++;
-        } while (name.length() > 30 || name.length() < 1);
-        loops = 0;
+        name = getString("Please enter between 1 and 30 characters: ", 1, 30);
 
         //Get GPA
         System.out.println("Enter a GPA: ");
-        do {
-            if (loops > 1) {
-                System.out.println("Please enter a number between 0.0 and 4.0 characters: ");
-            }
-            GPA = in.next();
-            loops++;
-        } while (Float.parseFloat(GPA) > 4.0 || Float.parseFloat(GPA) < 0.0);
-        loops = 0;
+        GPA = getFloat("Please enter a number between 0.0 and 4.0 characters: ", 0.0f, 4.0f);
 
         //Get major
         System.out.println("Enter a major: ");
-        do {
-            if (loops > 1) {
-                System.out.println("Please enter between 1 and 30 characters: ");
-            }
-            major = in.next();
-            loops++;
-        } while (major.length() > 30 || major.length() < 1);
-        loops = 0;
+        major = getString("Please enter between 1 and 30 characters: ", 1, 30);
 
         //Get year of graduation
         System.out.println("Enter a year of graduation: ");
-        do {
-            if (loops > 1) {
-                System.out.println("Please enter a number between 1912 and 2020 characters: ");
-            }
-            grad = in.next();
-            loops++;
-        } while (Integer.parseInt(grad) > 2018 || Integer.parseInt(grad) < 1900);
+        grad = getInt("Please enter a year between 1912 and 2020: ", 1900, 2018);
 
         //Construct the sqlQuery and execute it
         String sqlQuery = "INSERT INTO student (name, GPA, major, grad)\n" +
@@ -185,7 +212,20 @@ public class JDBCManipulate {
         }
     }
 
-    static void deleteEntry() {
+    static void deleteEntry(Statement sqlStatement) {
+        String name, GPA, major, grad;
+        String caller = "delete";
+        Scanner in = new Scanner(System.in);
+        int choice = 0;
+
+        String sqlQuery = "DELETE FROM student WHERE name = 'joe' " +
+                "ORDER BY timestamp_column LIMIT 1;";
+
+        try {
+            sqlStatement.executeUpdate(sqlQuery);
+        } catch (SQLException se) {
+            se.printStackTrace();
+        }
         System.out.println("Delete");
     }
 }//end JDBCManipulate
